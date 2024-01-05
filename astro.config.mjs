8fbdaf4e899117c03110fa164ccfcca0dbf1ee19
Maude Lemaire <maude.codes@gmail.com>
2024-01-05 13:35:47 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://qcmaude.github.io',
-  base: '/maudecodes',
+  site: 'https://maudethecodetoad.com',
   integrations: [mdx(), svelte()],
   markdown: {
     shikiConfig: {
